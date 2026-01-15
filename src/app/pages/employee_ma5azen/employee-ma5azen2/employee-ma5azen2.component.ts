a516ee44c107f11e40db45fa146e43e4e12f3771
Ereny <erenymahrous123@gmail.com>
2026-01-15 04:59:07 +0200

    employee ma5azen done

--- src/app/pages/employee_ma5azen/employee-ma5azen2/employee-ma5azen2.component.ts
+++ src/app/pages/employee_ma5azen/employee-ma5azen2/employee-ma5azen2.component.ts
@@ -3,14 +3,16 @@ import { HeaderComponent } from '../../../components/header/header.component';
 import { FooterComponent } from '../../../components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { AdditionsService } from '../../../services/additions.service';
-
+import { FormsModule } from '@angular/forms'; // ✅ هنا
 @Component({
   selector: 'app-employee-ma5azen2',
   imports: [
     HeaderComponent,
     FooterComponent,
-    CommonModule
+    CommonModule,
+      FormsModule // ✅ أضفناه هناs
   ],
+  standalone: true,
   templateUrl: './employee-ma5azen2.component.html',
   styleUrl: './employee-ma5azen2.component.css'
 })
@@ -18,6 +20,8 @@ export class EmployeeMa5azen2Component implements OnInit {
   userName: string = '';
   displayName: string = '';
   additions: any[] = [];
+searchCode: string = ''; // هذا للبحث
+filteredAdditions: any[] = [];
 
   private additionsService = inject(AdditionsService);
 
@@ -32,15 +36,27 @@ export class EmployeeMa5azen2Component implements OnInit {
     if (!fullName) return '';
     return fullName.trim().split(/\s+/).slice(0, 2).join(' ');
   }
-
-  loadAdditions(): void {
-    this.additionsService.getAllAdditions().subscribe({
-      next: (data) => {
-        this.additions = data;
-      },
-      error: (err) => {
-        console.error('خطأ في جلب البيانات', err);
-      }
-    });
+applyFilter(): void {
+  if (!this.searchCode) {
+    this.filteredAdditions = this.additions;
+  } else {
+    const code = this.searchCode.toLowerCase();
+    this.filteredAdditions = this.additions.filter(a => 
+      a.additionCode.toString().toLowerCase().includes(code)
+    );
   }
+}
+
+ loadAdditions(): void {
+  this.additionsService.getAllAdditions().subscribe({
+    next: (data) => {
+      this.additions = data;
+      this.applyFilter(); // فلترة أولية
+    },
+    error: (err) => {
+      console.error('خطأ في جلب البيانات', err);
+    }
+  });
+}
+
 }
